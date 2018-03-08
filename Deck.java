d21244b3f5057c5ff0ae6d92dfb009f0eed622bd
davidparks11 <davidparks11@gmail.com>
2018-03-08 13:16:53 -0600

    Deck methods and improvements to Card
    Deck:
    Implementation of Shuffle().
    Rework of getCard() (previously getValue()).
    Addition of getValueAt() and getSize().
    
    Card:
    Fix logic error in isFaceCard()

--- Deck.java
+++ Deck.java
@@ -39,13 +39,54 @@ public class Deck
     }
   }
   
-  public Card getValue(int index) //return card at given index. Needs work
+  public Card getCard(int index) //return card at given index. Needs work
   {
+    if (index < 0 || index >= getSize()) //handling for index out of bounds
+    {
+      System.out.println("ERROR - Invalid input: " + index);
+      return null;
+    }
     return deck[index];
   }
 
+  public int getValueAt(int index)
+  {
+    return getCard(index).getValue();
+  }
 
-  
+  public void shuffle() //shuffle method. Swaps cards and simulates deck cut
+  {
+    Card swap; //card holder
+    int numSwaps = 300; //number of swaps. 300 is arbitrary - change if needed.
+    Card[] temp = new Card[getSize()]; //deck holder
+    int randIndx = 51; //sudoRandom number for index position
+    for(int i = 0; i <= numSwaps; i++) //swaps amount in numSwaps. Cuts on last swap.
+    {
+      randIndx = (int) (Math.random()*getSize()); //sudoRandom number from 0 to deck size
+      swap = deck[randIndx]; //temp card holds random indexed value
+      deck[randIndx] = deck[i%getSize()]; //random index is given i mod deck size (so all cards will swap)
+      deck[i%getSize()] = swap; //index of card swapped is set to temp card - swap is complete
+      if(i == numSwaps) // cut done after last swap
+      {
+        for(int i2 = 0; i2 < getSize() - randIndx; i2++) //iters amount of cards to be moved to bottom of deck
+        {
+          temp[i2] = deck[randIndx + i2]; //places index to getSize() on bottom of deck
+        }
+        for(int i2 = 0; i2 < randIndx; i2++)
+        {
+          temp[getSize() - randIndx + i2] = deck[i2]; //places former bottom cards on top
+        }
+        for(int i2 = 0; i2 < getSize(); i2++)
+          deck[i2] = temp[i2]; //setting deck to temp
+      }
+    }
+  }
+
+  public int getSize()
+  {
+    return size; //returns deck size
+  }
+ 
   @Override
   public String toString() //overriding to String for correct print format
   {
@@ -54,6 +95,4 @@ public class Deck
     outPut += (current.toString() + "\n");
    return outPut;
   }
-
-
 }
